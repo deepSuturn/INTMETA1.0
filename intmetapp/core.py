b3cc77714ca51a79e5ff5e61cfdfb8e194e6e8af
JoseJuniorTK <69799917+JoseJuniorTK@users.noreply.github.com>
2022-03-04 20:05:08 -0300

    Update core.py

--- intmetapp/core.py
+++ intmetapp/core.py
@@ -32,25 +32,24 @@ def kraken(fileinput, taxonomiclevel):
 
     output_df = output_df.rename(columns={'Info0': 'Info2', 'Info5': 'Info1'}, inplace=False)
 
-    # Descomentar codigo para girar o dataframe caso for necessario em outra formatacao de visualizacao
-    # output_df = output_df.transpose()
-
-    # Pega apenas os 30 com maior numero de reads.
+    # Pega apenas os 10 com maior numero de reads.
     output_df_out = output_df.nlargest(10, 'Info2')
-    # output_df_out.reset_index(drop=True, inplace=True)
 
-
-    # Caso a output seja girada, substituir Index por true e header por false
+    # Transforma o dataframe para dicionário que será exportado como JSON para o D3JS
     dfd3 = output_df_out.to_dict('r')
+    
+    # Retira maior porcentagem para ser parametro de tamanho do gráfico
     maxpercent = output_df_out['Info2'].iloc[0]
     return dfd3, maxpercent
 
 def clark(fileinput, taxonomiclevel):
-    # Leitura inicial de dados
+    # Clark não possui parametro de escolha de nivel taxonomico em sua output, logo valor é descartado.
     dummydata = taxonomiclevel
+
+    # Leitura inicial de dados
     df_data = pd.read_csv(fileinput, low_memory=False, sep=',')
 
-    # Adiciona cabecalho para cada coluna para o output Metamaps.
+    # Adiciona cabecalho para cada coluna para o output Clark.
 
     # Info0 se refere ao Nome
     # Info1 se refere ao TaxID
@@ -65,27 +64,27 @@ def clark(fileinput, taxonomiclevel):
     output_df = df_data[['Info0', 'Info5']].copy()
 
     # Substituição dos cabecalhos para melhor visualizacao no D3.JS
+    # Segue a logica (Original:Saida_D3JS)
     output_df = output_df.rename(columns={'Info5': 'Info2', 'Info0': 'Info1'}, inplace=False)
 
-    # Descomentar codigo para girar o dataframe caso for necessario em outra formatacao de visualizacao
     # Se deleta uma linha do final do arquivo do CLARK
     output_df = output_df[:-1]
     
     # É necessária a conversão de uma tabela pra numérica
     output_df["Info2"] = output_df["Info2"].apply(pd.to_numeric)
 
-    # Pega apenas os 30 com maior numero de reads.
+    # Pega apenas os 10 com maior numero de porcentagem.
     output_df_out = output_df.nlargest(10, 'Info2')
     
-    # Retira maior porcentagem
+    # Retira maior porcentagem para ser parametro de tamanho do gráfico
     maxpercent = output_df_out['Info2'].iloc[0]
-
-    # Caso a output seja girada, substituir Index por true e header por false
-    print(output_df_out)
+    
+    # Transforma o dataframe para dicionário que será exportado como JSON para o D3JS
     dfd3 = output_df_out.to_dict('r')
     
     return dfd3, maxpercent
 
+# DESCONSIDERAR METAMAPS, É NECESSÁRIO ATUALIZAR.
 def metamaps(fileinput, taxonomiclevel):
     # Leitura inicial de dados
     df_data = pd.read_csv(fileinput, header=None, low_memory=False, sep='\t')
@@ -114,7 +113,7 @@ def metamaps(fileinput, taxonomiclevel):
     # Descomentar codigo para girar o dataframe caso for necessario em outra formatacao de visualizacao
     # output_df = output_df.transpose()
 
-    # Pega apenas os 30 com maior numero de reads. (Esse comando precisa ser adaptado ao metamaps)
+    # Pega apenas os 10 com maior numero de porcentagem. (Esse comando precisa ser adaptado ao metamaps)
     # output_df_out = output_df.nlargest(30, 'Sample')
 
     # Caso a output seja girada, substituir Index por true e header por false
